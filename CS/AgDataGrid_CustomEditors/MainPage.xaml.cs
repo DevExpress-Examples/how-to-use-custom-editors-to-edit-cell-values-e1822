8d33c06a05f7ccec2f429d25cf91d383bdabe484
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_CustomEditors/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace AgDataGrid_CustomEditors {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+    }
+}
---
+++ VB/AgDataGrid_CustomEditors/MainPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+
+Namespace AgDataGrid_CustomEditors
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = ProductList.GetData()
+		End Sub
+	End Class
+End Namespace
